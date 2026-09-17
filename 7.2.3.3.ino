771ed2f681aeeaac3eec1ad5a18b32f3925b6cfa
brianvo282 <brianvo282@gmail.com>
2026-09-17 14:03:27 -0700

    let me take you on a little trip

---
+++ 7.2.3.3.ino
@@ -0,0 +1,43 @@
+const int trigPin = 11;
+const int echoPin = 12;
+
+long duration;
+int distance;
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial.begin(9600);
+}
+
+int get_distance() {
+  int toReturn = 0;
+
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  toReturn = duration * 0.034 / 2;
+
+  return toReturn;
+}
+
+void loop() {
+  while (get_distance() > 5) {
+    Serial.println("Waiting to Detect An Object");
+    delay(100);
+  }
+  Serial.println("Object Detected & Present");
+
+  while (get_distance() < 5) {
+    delay(100);
+  }
+  Serial.println("Detected Object Is Gone");
+}
